835ce38dc354ff6449ffae4a943e6b2c8aef48fb
edowaye <edowayeadenaiya@gmail.com>
2022-09-12 08:39:59 -0700

    Fixed code error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 {
-	printf("n is positive\n");
+	printf("%d is positive", n);
 }
 	if (n == 0)
 {
-	printf("n is zero\n");
+	printf("%d is zero", n);
 }
 	if (n < 0)
 {
-	printf("n is negative\n");
+	printf("%d is negative", n);
 }
 	return (0);
 
