055d33a7988670be2edb141d7b53670ca5384570
edowaye <edowayeadenaiya@gmail.com>
2022-09-12 11:07:26 -0700

    Wrote a program that prints alphabets in lowercase

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Program to print last digit of num stored in n
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	printf("Last digit of %d is"
+
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Program is to printthe alphabet in lowercase'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar (ch);
+
+	return (0);
+}
+
+
