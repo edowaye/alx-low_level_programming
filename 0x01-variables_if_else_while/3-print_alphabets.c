525b0b30c6821f21c40f394abb30c8da634648de
edowaye <edowayeadenaiya@gmail.com>
2022-09-12 12:10:41 -0700

    Fixed error in code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Program to print in lower and upper case'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	
+		putchar (ch);
+	
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar (ch);
+	
+	putchar ('\n')
+	
+	return (0);
+}
+
