24724fe9bac0faab366659849016f579db97ba26
edowaye <edowayeadenaiya@gmail.com>
2022-09-16 16:58:45 -0700

    Fixed error in code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-  more_numbers - Prototype function
+  *more_numbers - Prototype function
   *
   *Description: A function that prints 10 times the numbers,
   *from 0 to 14, followed by a new line
   *
   *Return: 0
-i  */
+ */
 
 void more_numbers(void)
 {
