27a9c9492b5be4801a1c1e9f90fbeec77bc7ceee
edowaye <edowayeadenaiya@gmail.com>
2022-09-16 09:01:27 -0700

    Added the code for printing alphabets in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Function to print the alphabet
+ * in lowercase 10 times
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
