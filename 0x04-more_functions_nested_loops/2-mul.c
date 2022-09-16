640e31145fee9491fd4fc0de365edc50c39c7f5c
edowaye <edowayeadenaiya@gmail.com>
2022-09-16 16:02:54 -0700

    Added afunction that muliplies to integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *mul - Prototype function
+  *
+  *Description - Function that multiplies two integers
+  *
+  *@a: First input
+  *@b: Second input
+  *
+  *Return: 0
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
