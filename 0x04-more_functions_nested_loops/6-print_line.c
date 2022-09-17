57e803ee1ebc3fbbe972bba269264b309660890e
edowaye <edowayeadenaiya@gmail.com>
2022-09-16 17:06:56 -0700

    Fixed errors caused by betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,8 +3,8 @@
 
 /**
  * print_line - a function that draws a straight line on screen
- @n: input number of times to print '_'
-*Return: a straight line
+ *@n: Length of line
+ *Return: a straight line
  */
 
 void print_line(int n)
