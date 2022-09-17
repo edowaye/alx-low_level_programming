db377e1a6b9e2e93f1afe9d9590f48017c0db4fe
edowaye <edowayeadenaiya@gmail.com>
2022-09-16 17:45:09 -0700

    Fixed errors

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,19 +14,16 @@ void print_diagonal(int n)
 {
 	int x, y;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (x = 0; x <= n; x++)
+		for (x = 0; x < n; x++)
 		{
-			for (y = 0; y < n; y++)
+			for (y = 0; y < (n - 1); y++)
 			{
-				_putchar(' ');
+				_putchar('#');
 			}
-			_putchar('\\');
+			_putchar('#');
+
 			_putchar('\n');
 		}
 	}
