9db8aca2a122aa811f829298775d8df175f0d9c2
edowaye <edowayeadenaiya@gmail.com>
2022-09-17 12:12:43 -0700

    Added a missing asterisk to my code

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-  *Add - two integers and returns the result
+  *add - two integers and returns the result
   *
   *Description: Function that adds two integers
   *and returns the result
   *@one: Input 1
-  @two: Input 2
+  *@two: Input 2
   *
   *Return: one plus two
   */
 
 int add(int one, int two)
 {
-	return(one + two);
+	return (one + two);
 }
