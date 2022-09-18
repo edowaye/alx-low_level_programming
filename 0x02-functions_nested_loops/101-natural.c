2e1e2d3ae1acff097624b11a62a5a2ddae4fff1c
edowaye <edowayeadenaiya@gmail.com>
2022-09-18 07:41:27 -0700

    Wrote a function that sums up the multiples of 3 and 5 between 0 and 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *
+  *Description: Function that links natural
+  *numbers and sums up
+  *
+  */
+
+int main(void)
+{
+	int x, y = 0;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if ((x % 5) == 0 || (x % 3) == 0)
+
+			y += x;
+	}
+	printf("%d\n", y);
+	return (0);
+}
