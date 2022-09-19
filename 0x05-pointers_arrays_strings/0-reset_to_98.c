c5a6906fe5551dbaa9a1bdfa33350a35f635e9fb
edowaye <edowayeadenaiya@gmail.com>
2022-09-19 10:38:35 -0700

    Assigned value to int:

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *reset_to_98 - Prototype function
+  *
+  *Description - Function that takes a pointer to an int as
+  *parameter and updates the integer to 98
+  *@n - Input
+  *Return: Nothing
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
