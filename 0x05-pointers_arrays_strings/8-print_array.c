e1a4e2416bfa2fb39e53cac2c7f42cf8c19c92fe
edowaye <edowayeadenaiya@gmail.com>
2022-09-20 14:02:39 -0700

    fixed compilation errors

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,5 +12,15 @@
 
 void print_array(int *a, int n)
 {
-	int 
+	int array[] = n;
 
+	for (int length = sizeof(arr) / sizeof(arr[0]);
+			printf("Elements of given array: \n");
+
+			for (int i = 0; i < length; i++)
+			{
+			printf("%d ", arr[i]);
+			}
+
+			return (0);
+}
