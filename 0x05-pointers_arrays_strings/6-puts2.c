bbba77ea71b11555b98555795acdbc153fa132e4
edowaye <edowayeadenaiya@gmail.com>
2022-09-20 13:18:43 -0700

    Fixed eros in compilation

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,40 @@
 #include "main.h"
+#include <string.h>
+/**
+  *_strelen - Prototype function
+  *@s: string whose length is to be returned
+  *
+  *Return: Count of char as an int
+  */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
 
 /**
   *puts2 - Function prototype
   *
-  *@str: string to be printed
+  *@s: string to be printed
   *
   *Return: 0
   */
 
-void puts2(char *str)
+void puts2(char *s)
 {
-	int len = strlen(s);
+	int i = 0;
 
-
+	while (s[i] != '\n' && s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
