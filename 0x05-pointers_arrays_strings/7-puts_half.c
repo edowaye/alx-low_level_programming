79938208402db8a44216e2aff5b6d4a61b3c565f
edowaye <edowayeadenaiya@gmail.com>
2022-09-21 05:41:55 -0700

    Created a file containing a function that prints the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,37 @@
 #include "main.h"
-#inlcude <string.h>
+#include <string.h>
 
 /**
   *puts_half - Prototype function
   *
-  *@n: number of characters in string
+  *@str: string to print
   *
   *Return: 0
   */
 
 void puts_half(char *str)
 {
-	char c;
-	int i;
+	int i, len = _strlen(str);
 
-	for (int i = 0;
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		_putchar(*(str + i));
+	_putchar(10);
+}
+
+
+/**
+  *_str-len: Prototype function
+  *
+  *@s:string
+  *
+  *Return: the length of string
+  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+			len++;
+	return (len);
+}
