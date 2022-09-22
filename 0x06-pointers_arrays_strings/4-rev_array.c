e410e6d8cf8f598989257680dc345933a92e9f9d
edowaye <edowayeadenaiya@gmail.com>
2022-09-22 12:49:18 -0700

    fixed error

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -18,13 +18,11 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n; i++)
 		p++;
 
-	for (k = 0, k < n / 2; k++)
+	for (k = 0, k < n / 2; k++;)
 	{
 		aux = a[k];
 		a[k] = *p;
 		*p = aux;
 		p--;
-
-		return (0);
 	}
 }
