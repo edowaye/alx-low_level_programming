82d9628dd786770e44b4307ebf152e3d1e2cb5bf
edowaye <edowayeadenaiya@gmail.com>
2022-09-22 07:29:17 -0700

    Created a file for concatenating 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+int _strlen(char *s)
+
+/**
+  * *_strcat - Function prototype
+  *
+  *@dest: destination str
+  *@src: source str
+  *
+  *Return:str
+  */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; x++)
+		count +=1;
+
+	return (count);
+ }
+
+
+char *_strcat(char *dest, char *src)
+{
+
+	char dest[] = x;
+	char src[] = y;
+
+	_strcat(dest, src);
+
+	_putchar("%s\n", dest);
+
+	return (0);
+	}
