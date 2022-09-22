19de7933fe0fb884eb85a1748c3d796e5e4c22cf
edowaye <edowayeadenaiya@gmail.com>
2022-09-22 12:55:22 -0700

    fixed errors

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,7 +6,6 @@
  *an array
  *@a: array
  *@n: integer from array
- *
  */
 
 void reverse_array(int *a, int n)
@@ -14,11 +13,9 @@ void reverse_array(int *a, int n)
 	int *p, i, aux, k;
 
 	p = a;
-
 	for (i = 0; i < n; i++)
 		p++;
-
-	for (k = 0, k < n / 2; k++;)
+	for (k = 0; k < n / 2; k++)
 	{
 		aux = a[k];
 		a[k] = *p;
