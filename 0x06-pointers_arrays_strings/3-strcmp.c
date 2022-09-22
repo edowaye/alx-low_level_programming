055c042779c58468efb01d5ef578eee6dc8668e5
edowaye <edowayeadenaiya@gmail.com>
2022-09-22 12:45:42 -0700

    Added a function to reverse the contents of elemnts of an array

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strcmp - Function prototype
+  * @s1 - parameter
+  * @s2 - parameter
+  * Return - an integer
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *reverse_array - Function to reverse
+ *an array
+ *@a: array
+ *@n: integer from array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+
+	for (i = 0; i < n; i++)
+		p++;
+
+	for (k = 0, k < n / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+
+		return (0);
+	}
+}
