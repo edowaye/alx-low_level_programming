e0297dd86c513c3bba746911066f06507e15aeda
edowaye <edowayeadenaiya@gmail.com>
2022-09-25 07:10:56 -0700

    fixed errors in my code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,14 +15,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, " * (a + i));
+		printf("%d", a[i]);
 
-		if (i != (n - 1))
-			printf(" ,");
-		else /*if (i = (n - 1)*/
-			break;
+		if (i == (n - 1))
+			continue;
+			printf(", ");
 	}
 	printf("\n");
 }
