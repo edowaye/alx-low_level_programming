146532cfe72e991386a1ea20164638c9b72ebf45
edowaye <edowayeadenaiya@gmail.com>
2022-09-25 07:49:23 -0700

    Added a file with program that converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  *_atoi - Function prototype
+  *@s: a paramete9r
+  *return: an integer
+  */
+
+int _atoi(char *s)
+{
+	int i = 1;
+	unsigned int u = 0;
+	char ch;
+
+	while (*s)
+	{
+		if (*s == '-')
+			i *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			ch = 1;
+			u = u * 10 + *s - '0';
+		}
+
+		else if (ch)
+			break;
+		s++;
+
+	if (i < 0)
+		u = (-u);
+
+	return (u);
+	}
+}
