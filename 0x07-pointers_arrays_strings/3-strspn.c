88faf22820a1e07b6b9eaccf99c495ca6e85de88
edowaye <edowayeadenaiya@gmail.com>
2022-09-27 13:17:45 -0700

    fixed errors in code

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,9 +6,30 @@
   *@s: string to be checked
   *@accept: string containing characters to be matched
   *
-  *Return: ptr to char in str
+  *Return: number of bytes in initial str
   */
 
-char *_strpbrk(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int r = 0;
+	int i, j;
 
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			j = 0;
+
+			if (*s == accept[i])
+			{
+				r++;
+				j = 1;
+				break;
+		}
+	}
+		if (!j)
+			break;
+		s++;
+	}
+	return (r);
+}
