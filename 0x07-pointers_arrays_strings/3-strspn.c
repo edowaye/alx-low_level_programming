d6f0c7b9cc59acc6498140ddba00ebfbdd313994
edowaye <edowayeadenaiya@gmail.com>
2022-09-27 12:01:39 -0700

    Added a function that compiled successfully strpbk

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,4 +3,12 @@
 /**
   *_strspn - Function prototype
   *
+  *@s: string to be checked
+  *@accept: string containing characters to be matched
   *
+  *Return: ptr to char in str
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+
