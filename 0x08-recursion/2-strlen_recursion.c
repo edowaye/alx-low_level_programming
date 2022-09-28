af048e7afec98a20e7b1f86de7e2b2078c72b8ec
edowaye <edowayeadenaiya@gmail.com>
2022-09-28 15:59:16 -0700

    Added a string length recursive program

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_strlen_recursion - Function prototype
+  *
+  *@s: string whose length is being found
+  *Description: A function that returns the length
+  *of a string
+  *Return: a character
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
