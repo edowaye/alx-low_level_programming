188552a555569acea71d7b319e38cdf5b555648a
edowaye <edowayeadenaiya@gmail.com>
2022-09-28 16:11:26 -0700

    Added a factorial program using the recursive method

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *factorial - Function prototype
+  *
+  *Description: This program writes a function
+  *that eturns the factorial of a given number n
+  *@n: parameter
+  *Return: an integer
+  */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n >= 1)
+		return (1);
+}
