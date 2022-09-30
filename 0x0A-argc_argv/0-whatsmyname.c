467d51a1fab20f9f5a22fd9ea084259fe9642dd2
edowaye <edowayeadenaiya@gmail.com>
2022-09-30 11:46:03 -0700

    Added a main.h file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point of function that prints
+  *its name followed by a new line
+  *
+  *@argv: argument vector
+  *@argc: argument count
+  *
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  *main: header files for prototype
+  *Description - Inclusion of prototype functions
+  *
+  */
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
