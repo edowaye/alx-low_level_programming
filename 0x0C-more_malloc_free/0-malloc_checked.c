b2479ef394e2bfc93208c4f2095bea0c474364b1
edowaye <edowayeadenaiya@gmail.com>
2022-10-06 11:59:49 -0700

    Added my main.h file that contains all function prototypes

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+/**
+  *main - Entry point for a function
+  *that allocates memory using malloc
+  *
+  *
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+A new subdirectory under lowlevel programming on more malloc and free
