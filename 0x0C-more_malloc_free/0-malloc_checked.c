5ce6cc56bff28c2c08d30babfa6a17bf7f06e05e
edowaye <edowayeadenaiya@gmail.com>
2022-10-06 12:47:33 -0700

    Added a function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
 
 /**
-  *main - Entry point for a function
+  *malloc_checked - Entry point for a function
   *that allocates memory using malloc
   *
+  *@b: a parameter
   *
+  *Return: a pointer to alllocated mem
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+  *_putchar - Outputs the character c
+  *to standard output
+  *
+  *@c: character to output
+  *
+  *Return: 1 if successful
+  */
+
+int _putchar(char c)
+{
+	return (write(1 &c, 1))
+}
