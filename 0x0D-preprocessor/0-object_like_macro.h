0439d5fe3b699b2f92c0b9232f1bc7a2b1d372c7
edowaye <edowayeadenaiya@gmail.com>
2022-10-10 06:21:28 -0700

    Created a header file that defines a macro named SIZE as an abbreviation for the token 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef SIZE
+#define SIZE
+#define SIZE 1024
+/**
+  *main: header file for object-
+  *like micro
+  *Description - Inclusion of header file that
+  *defines a micro named size
+  *
+  */
+
+#endif /*SIZE*/
