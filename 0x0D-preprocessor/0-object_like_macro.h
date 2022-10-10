ffb075b773c76e895877921421e53c0ed7e5adbf
edowaye <edowayeadenaiya@gmail.com>
2022-10-10 06:23:03 -0700

    Made adjustments in main

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,7 +2,7 @@
 #define SIZE
 #define SIZE 1024
 /**
-  *main: header file for object-
+  *main - header file for object-
   *like micro
   *Description - Inclusion of header file that
   *defines a micro named size
