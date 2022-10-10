8ae68a78f798d777c799e89072f10c5588a1f0c7
edowaye <edowayeadenaiya@gmail.com>
2022-10-10 07:07:13 -0700

    wROTE a function-like macro ABS(x) that computes the absolute value of a number x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS
+#define ABS X
+#define (-X) (X)
+
+/**
+  *main: header file for absolute value
+  *Description - Inclusion of a function-like macro
+  *that computes absolute value of an integer
+  *
+  */
+
+#endif /*ABS*/
