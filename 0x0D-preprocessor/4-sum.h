be84d19abb4d783e24da7b19f8dacafbbc457961
edowaye <edowayeadenaiya@gmail.com>
2022-10-10 07:28:01 -0700

    Wrote a function-like macro SUM(x, y) that computes the sum of the numbers x and y.

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM
+#define SUM
+#define SUM(x, y) ((x) + (y))
+
+/**
+  *main - Entry point
+  *
+  *Description - Inclusion on function-like micro
+  *that computes the sum of the numbers x and y
+  *
+  */
+
+#endif /*SUM*/
