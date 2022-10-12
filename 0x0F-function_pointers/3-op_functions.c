13f9e88b07d4c128b3b4e258171dacb45d957ad0
edowaye <edowayeadenaiya@gmail.com>
2022-10-12 12:14:40 -0700

    Renamed file from 3getops to 3opfunctions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,87 @@
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include "3-calc.h"
+
+/**
+  *op_add - function prototype that returns
+  *sum of integers
+  *
+  *@a: integer 1
+  *@b: integer 2
+  *
+  *Return: integer sum
+  */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  *op_sub: function prototype that returns
+  *difference of integers
+  *
+  *@a: integer 1
+  *@b: integer 2
+  *
+  *Return: integer difference
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  *op_mul - function prototype that returns
+  *product of integers
+  *
+  *@a: integer 1
+  *@b: integer 2
+  *
+  *Result: integer produce
+  */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  *op_div - function prototype that returns
+  quotient of integers
+  *
+  *@a: integer 1
+  *@b: integer 2
+  *
+  *Result: integer quotient
+  */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+  *op_mod - function prototype that returns
+  *remainder of integers
+  *
+  *@a: integer 1
+  *@b: integer 2
+  *
+  *Result: integer remainder
+  */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+retur
