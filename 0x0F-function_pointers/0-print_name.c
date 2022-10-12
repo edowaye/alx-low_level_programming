3bb5803e374116b1fafe6227b538692df2ae623e
edowaye <edowayeadenaiya@gmail.com>
2022-10-12 06:11:27 -0700

    Added a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *print_name - prototype function that prints
+  *a name
+  *
+  *@name: pointer to output to be printed
+  *@f: function pointer
+  *
+  *Return: Nothing (void)
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
