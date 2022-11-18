5c82a66b8141d36be6472889cb1b956f2db15ca4
edowaye <edowayeadenaiya@gmail.com>
2022-11-18 07:40:28 -0800

    Updated code

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,34 +6,19 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
+
+void print_number(int n)
 {
-	long int n;
-	long int max;
-	long int i;
 
-	n = 612852475143;
-	max = -1;
+	unsigned int x;
 
-	while (n % 2 == 0)
+	x = n;
+	if (n < 0)
 	{
-		max = 2;
-		n /= 2;
+		_putchar(45);
+		x = -n;
 	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
-
-	return (0);
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
